5519437a5e994f8ef2e129cd09bed73ff237205f
Ofek Bashan <ofek.bashan@genesys.com>
2020-06-06 10:20:38 +0300

    More tests

--- test/services/messageService.test.js
+++ test/services/messageService.test.js
@@ -30,4 +30,56 @@ describe("testing addMessage", () => {
         expect(error).toBeInstanceOf(ValidationError);
         expect(error.message.startsWith("Parameter message")).toBe(true);
     });
+
+    test('throws ValidationError when message is not a string', async() => {
+        let error;
+        try {
+            await messagesService.addMessage(1234, 5);
+        }
+        catch(err) {
+            error = err;
+        }
+        expect(error).toBeDefined();
+        expect(error).toBeInstanceOf(ValidationError);
+        expect(error.message.startsWith("Parameter message")).toBe(true);
+    });
+
+    test('throws ValidationError when time is not a number', async() => {
+        let error;
+        try {
+            await messagesService.addMessage("wrong_time", "valid message");
+        }
+        catch(err) {
+            error = err;
+        }
+        expect(error).toBeDefined();
+        expect(error).toBeInstanceOf(ValidationError);
+        expect(error.message.startsWith("Parameter time")).toBe(true);
+    });
+
+    test('throws ValidationError when time is a negative number', async() => {
+        let error;
+        try {
+            await messagesService.addMessage(-4443413, "valid message");
+        }
+        catch(err) {
+            error = err;
+        }
+        expect(error).toBeDefined();
+        expect(error).toBeInstanceOf(ValidationError);
+        expect(error.message.startsWith("Parameter time")).toBe(true);
+    });
+
+    test('throws ValidationError when time already passed', async() => {
+        let error;
+        try {
+            await messagesService.addMessage(new Date().getTime() - 1, "valid message");
+        }
+        catch(err) {
+            error = err;
+        }
+        expect(error).toBeDefined();
+        expect(error).toBeInstanceOf(ValidationError);
+        expect(error.message.startsWith("Parameter time")).toBe(true);
+    });
 });
